ca208cb6fc253af59b093ab42cac53a794e7dadf
Abdallah Ahmed <abdallaahmedabdash@gmail.com>
2022-06-03 16:19:06 -0700

    adding comments in c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /*
  * main-code entry point
- *Description: Printing "Programming is like building a multilingual puzzle"
+ *
  * Return: Always 0 (Success)
+ * 
  */
+
+
 int main(void)
 
 
