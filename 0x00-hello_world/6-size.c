84a9d38747d54c453e29a91a4899b7a7a8e769c0
Abdallah Ahmed <abdallaahmedabdash@gmail.com>
2022-06-03 17:24:41 -0700

    errors fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,13 +2,13 @@
 
 /**
  *
- * *main - Entry point
+ *main - Entry point
  *
- * *Descripton: "prints the size of C data type files"
+ *Descripton: "prints the size of C data type files"
  *
- * *Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  *
- * */
+ */
 
 int main(void)
 
@@ -28,13 +28,13 @@ int main(void)
 
 
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of a char: %zu byte(s)\n", sizeof(d));
 
-	printf("Size of an int: %zu byte(s)\n", sizeof(b));
+	printf("Size of an int: %zu byte(s)\n", sizeof(a));
 
-	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(b));
 
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(c));
 
 	printf("Size of a float: %zu byte(s)\n", sizeof(e));
 
