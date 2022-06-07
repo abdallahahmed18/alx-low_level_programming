1f9178184f360fb36c2e66103d44ad048634c9d5
Abdallah Ahmed <abdallaahmedabdash@gmail.com>
2022-06-07 15:44:15 -0700

    program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,20 +9,14 @@
 
 
 int main(void)
-
 {
+	char x[] = "_putchar";
+	int i;
 
-		char x[] = "_putchar";
-
-		int i;
-
-		for (i = 0; i < 8; i++)
-		{
-				_putchar(x[i]);
-							}
-
-				_putchar('\n');
-
-				return (0);
-
+	for (i = 0; i < 8; i++)
+	{
+	_putchar(x[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
