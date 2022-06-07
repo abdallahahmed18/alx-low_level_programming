dbba4f857b78c5cc874d43b3b8788c633c1cf154
Abdallah Ahmed <abdallaahmedabdash@gmail.com>
2022-06-06 17:41:03 -0700

    fixing errors

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -52,7 +52,7 @@ int main(void)
 
 	{
 
-		printf("Last digit of %d is %d and is less than 6 and 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 
 	}
 
