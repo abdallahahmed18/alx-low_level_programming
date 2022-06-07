fc42d03fddd04aebe0745ad137207b634fc37713
Abdallah Ahmed <abdallaahmedabdash@gmail.com>
2022-06-06 17:37:02 -0700

    line editted for spacing

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,6 @@
 * Return: Always 0 (Success)
 *
 */
-
 int main(void)
 
 {
@@ -53,12 +52,10 @@ int main(void)
 
 	{
 
-		printf("Last digit of%d is%d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and 0\n", n, last_digit);
 
 	}
 
 	return (0);
 
 }
-
-
