34055cf98832beea02e12b70d68751bebb07db5b
abdallahahmed18 <abdallaahmedabdash@gmail.com>
2022-07-07 15:22:40 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * print_numbers - prints parameter-n ,' ' (0..n)-times
+ * @separator: comma space value between parameters
+ * @n: number of parameters
+ * Return: none
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (separator == NULL || *separator == '\0')
+			continue;
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	putchar('\n');
+	va_end(list);
+}
