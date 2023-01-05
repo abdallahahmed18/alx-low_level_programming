277ac67b71aab2dae831189dc2de8b9232063fb7
Abdallah Ahmed <abdallaahmedabdash@gmail.com>
2023-01-05 03:20:05 -0800

    tested

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,23 @@
+nclude "search_algos.h"
+
+
+
+/**
+ *
+ *  * recursive_search - searches for a value in an array of
+ *
+ *   * integers using the Binary search algorithm
+ *
+ *    *
+ *
+ *     *
+ *
+ *      * @array: input array
+ *
+ *       * @size: size of the array
+ *
+ *        * @value: value to search in
+ *
+ *         * Return: index of the number
+ *
+ *          */
